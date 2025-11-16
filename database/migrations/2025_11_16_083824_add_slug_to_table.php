9a6cc1e9542cebfcfb8c518bbf6c196d5aafed7b
hamdy emad <razereng0@gmail.com>
2025-11-16 10:40:47 +0200

    add migration for area settings

---
+++ database/migrations/2025_11_16_083824_add_slug_to_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('countries', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+        });
+        Schema::table('cities', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+        });
+        Schema::table('regions', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+        });
+        Schema::table('subregions', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('table', function (Blueprint $table) {
+            //
+        });
+    }
+};

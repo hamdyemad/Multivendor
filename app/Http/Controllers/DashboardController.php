f27f324b1496dee15393c30be84bed3e20bb02cf
mostafa42 <mostafagamalsaid475@gmail.com>
2025-11-20 15:49:07 +0200

    final notification

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,24 +8,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $vendor = auth()->user()->vendor;
-
-        $all_transactions = [];
-
-        if ($vendor) {
-            $all_transactions = Withdraw::with([ "vendor" => function($vendor){
-                $vendor->with("translations")->first();
-            }])->where('reciever_id', $vendor->id)
-                ->latest()
-                ->limit(10)
-                ->get();
-        } else {
-            $all_transactions = Withdraw::with([ "vendor" => function($vendor){
-                $vendor->with("translations")->first();
-            }])->latest()->limit(10)->get();
-        }
-
-        return $all_transactions;
         $data = [
             'title' => __('menu.dashboard.title'),
         ];

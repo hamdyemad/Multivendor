3100e60ce57cbc663f262ce75459f79ac96ddb1c
hamdy emad <razereng0@gmail.com>
2025-12-15 17:21:20 +0200

    done

--- Modules/Vendor/routes/api.php
+++ Modules/Vendor/routes/api.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Vendor\app\Http\Controllers\Api\VendorApiController;
 
-Route::middleware(['throttle:600,1'])->group(function () {
-    Route::apiResource('vendors', VendorApiController::class);
+Route::apiResource('vendors', VendorApiController::class);
 
-    // Vendor request endpoint
-    Route::post('vendor-request', [VendorApiController::class, 'vendorRequest']);
-});
+// Vendor request endpoint
+Route::post('vendor-request', [VendorApiController::class, 'vendorRequest']);
